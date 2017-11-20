d3b68200a5f57747f55541241ca855e7b842f97e
rush <johny64@gmail.com>
2017-11-20 11:38:09 +0300

    tests for guzzle adapter / added php 5.6 support

--- tests/Vscale/Adapter/GuzzleHttpAdapterTest.php
+++ tests/Vscale/Adapter/GuzzleHttpAdapterTest.php
@@ -30,4 +30,44 @@ class GuzzleHttpAdapterTest extends TestCase
 
         sleep(2);
     }
+
+    public function testPost()
+    {
+        $this->expectException('\InvalidArgumentException');
+        $this->expectExceptionMessage('[Vscale] Unknown error received: Client error: `POST https://api.vscale.io/v1/scalets` resulted in a `403 Forbidden` response');
+
+        $this->client->post('scalets', []);
+
+        sleep(2);
+    }
+
+    public function testPut()
+    {
+        $this->expectException('\InvalidArgumentException');
+        $this->expectExceptionMessage('[Vscale] Unknown error received: Client error: `PUT https://api.vscale.io/v1/scalets/tags/1` resulted in a `403 Forbidden` response');
+
+        $this->client->put('scalets/tags/1', []);
+
+        sleep(2);
+    }
+
+    public function testDelete()
+    {
+        $this->expectException('\InvalidArgumentException');
+        $this->expectExceptionMessage('[Vscale] Unknown error received: Client error: `DELETE https://api.vscale.io/v1/scalets/1` resulted in a `403 Forbidden` response');
+
+        $this->client->delete('scalets/1');
+
+        sleep(2);
+    }
+
+    public function testPatch()
+    {
+        $this->expectException('\InvalidArgumentException');
+        $this->expectExceptionMessage('[Vscale] Unknown error received: Client error: `PATCH https://api.vscale.io/v1/scalets/1/start` resulted in a `403 Forbidden` response');
+
+        $this->client->patch('scalets/1/start', []);
+
+        sleep(2);
+    }
 }
